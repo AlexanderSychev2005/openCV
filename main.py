0009074208ad1face24bea9ae4828ae6a08bfd5a
Alex <alexandersychev2005@gmai.com>
2023-05-28 13:29:25 +0300

    make a stroke

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import cv2
 
-img = cv2.imread('images/image.png')
+img = cv2.imread('images/image.jpg')
 
 img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+# img = cv2.GaussianBlur(img, (9, 9), 0)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
+img = cv2.Canny(img, 90, 90)
 # img[0:100, 0:150]
 cv2.imshow('Result', img)
 
