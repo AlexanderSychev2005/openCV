686518fca69c42ab1c82a7bb3577ea76c1e73375
Alex <alexandersychev2005@gmai.com>
2023-05-30 13:01:28 +0300

    Build contours

--- transformations.py
+++ transformations.py
@@ -1,23 +1,26 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture('videos/video.mp4')
-
-while True:
-    success, img = cap.read()
-
-    # img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
-    img = cv2.GaussianBlur(img, (9, 9), 0)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    img = cv2.Canny(img, 30, 30)
-
-    kernel = np.ones((5, 5), np.uint8)
-    img = cv2.dilate(img, kernel, iterations=1)
-
-    img = cv2.erode(img, kernel, iterations=1)
-    cv2.imshow('Result', img)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+img = cv2.imread("images/intro.jpg")
 
 
+# img = cv2.flip(img, 1)
+
+def rotate(img_param, angle):
+    height, width = img_param.shape[:2]
+    point = (width // 2, height // 2)
+
+    mat = cv2.getRotationMatrix2D(point, angle, 1)
+    return cv2.warpAffine(img_param, mat, (width, height))
+
+
+# img = rotate(img, 45)
+
+def transform(img_param, x, y):
+    mat = np.float32([[1, 0, x], [0, 1, y]])
+    return cv2.warpAffine(img_param, mat, (img_param.shape[1], img_param.shape[0]))
+
+
+img = transform(img, 30, 200)
+cv2.imshow("Result", img)
+cv2.waitKey(0)
